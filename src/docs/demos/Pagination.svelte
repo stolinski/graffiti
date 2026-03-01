5e8c1a36087d77835ab47320e6afab5af2e9d37b
Scott Tolinski <scott.tolinski@gmail.com>
2026-03-01 09:15:09 -0700

    Use .button.ghost instead of .button.ghost.mini for pagination

--- src/docs/demos/Pagination.svelte
+++ src/docs/demos/Pagination.svelte
@@ -1,10 +1,10 @@
 <nav class="pagination" aria-label="Pagination">
-  <a class="button ghost mini" href="/?page=1" aria-disabled="true">Previous</a>
+  <a class="button ghost" href="/?page=1" aria-disabled="true">Previous</a>
   <ul>
-    <li><a class="button ghost mini" href="/?page=1">1</a></li>
-    <li><a class="button ghost mini" href="/?page=2" aria-current="page">2</a></li>
-    <li><a class="button ghost mini" href="/?page=3">3</a></li>
-    <li><a class="button ghost mini" href="/?page=4">4</a></li>
+    <li><a class="button ghost" href="/?page=1">1</a></li>
+    <li><a class="button ghost" href="/?page=2" aria-current="page">2</a></li>
+    <li><a class="button ghost" href="/?page=3">3</a></li>
+    <li><a class="button ghost" href="/?page=4">4</a></li>
   </ul>
-  <a class="button ghost mini" href="/?page=3">Next</a>
+  <a class="button ghost" href="/?page=3">Next</a>
 </nav>
